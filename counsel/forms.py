3d75bd7a324a3646a35bc550a4f65263dcb7ea91
SongMihyun <idblackcat@gmail.com>
2024-10-04 16:13:55 +0900

    가족그룹 기능 추가

--- counsel/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from counsel.models import Counsel
-
-class CounselForm(forms.ModelForm):
-    class Meta:
-        model = Counsel
-        fields = ['category','description']
